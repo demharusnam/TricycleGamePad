80a89db316786138f50cf9d9f6976cb2dec369d5
Mansur Ahmed <ahmedm.dev@gmail.com>
2021-08-01 19:04:52 -0400

    Initial Commit

---
+++ TricycleGamePad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TricycleGamePad
+//
+//  Created by Mansur Ahmed on 2021-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TricycleGamePad/TricycleGamePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TricycleGamePadApp.swift
+//  TricycleGamePad
+//
+//  Created by Mansur Ahmed on 2021-08-01.
+//
+
+import SwiftUI
+
+@main
+struct TricycleGamePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
